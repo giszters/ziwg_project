559b7d503f24363b8b562a85edbca31e0f98e7de
Mateusz <giszters@gmail.com>
2014-12-21 18:35:13 +0100

    add first version of order edit

--- hotelix/client/views.py
+++ hotelix/client/views.py
@@ -3,8 +3,21 @@
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from client.models import Client
+
+from hotelix.views import SuccessMixin
+from client.models import Client, Order
 
 
 class ClientList(ListView):
     model = Client
+
+
+## inherit also StructureMixin, SuccessMixin?
+class OrderEdit(SuccessMixin, UpdateView):
+    model = Order
+    template_name = "edit.html"
+    bs_length = 1
+
+
+class OrderList(ListView):
+    model = Order
